9d8a06891717ebf2e9803e7e7610306095f93901
jion_kozono2 <jion_kozono@mates-system.com>
2026-03-19 18:17:14 +0900

    update: dotfiles

--- mise/_mise.sh
+++ mise/_mise.sh
@@ -4,5 +4,6 @@ set -eu
 
 which mise >/dev/null 2>&1 && mise use --global python@latest
 which mise >/dev/null 2>&1 && mise use --global golang@latest
+which mise >/dev/null 2>&1 && mise use --global node@latest
 
 echo "Install mise plugins is Done!"
